0dd5d4b3c9819871c8d0dcdfa24f94045ada6db3
Kaixexn <impartialbgg2@gmail.com>
2026-08-07 00:22:24 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        dswdBlue: '#002B66',
+        dswdRed: '#D32F2F',
+        dswdYellow: '#FBC02D',
+        dswdLight: '#F4F6F9'
+      },
+      fontFamily: {
+        body: ['Inter', 'system-ui', 'sans-serif'],
+        heading: ['"Source Serif 4"', 'Georgia', 'serif']
+      }
+    }
+  }
+}
